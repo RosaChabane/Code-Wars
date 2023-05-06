3d3cf396e056060f9821f2a2c28497affee131dc
Rosa Chabane <rmiliziano@yahoo.com>
2023-05-06 16:12:06 -0500

    added my solution to reduce-but-grow.js

--- 8-kyu/Reduce-But-Grow.js
+++ 8-kyu/Reduce-But-Grow.js
@@ -3,3 +3,17 @@
 // [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 
 //My solution:
+
+function multiplyArray(arr) {
+    let product = 1;
+	for (let i = 0; i < arr.length; i++) {
+		product *= arr[i];
+	}
+    return product;
+}
+
+console.log(multiplyArray([1, 2, 3, 4]));
+// expected output: 24
+
+console.log(multiplyArray([2, 22, 4, 6]));
+// expected output : 1056
