b6a1b4d2f6737e2d4fd542c44d99f1c901a7ba76
Rosa Chabane <rmiliziano@yahoo.com>
2023-07-01 22:49:14 -0500

    added Twice-As-Old code-wars 8-kyu challenge

---
+++ 8-kyu/Twice-As-Old.js
@@ -0,0 +1,21 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). 
+// The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+
+//My Solution:
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let doubleAge = dadYearsOld - sonYearsOld * 2;
+    return doubleAge < 0 ? doubleAge * -1 : doubleAge;
+}
+
+
+//My Explanation:
+//to determine how many years ago the father was twice his son's age, the son's age * 2 can be subtracted from the father's age
+//firstly, assign the difference of son'a age * 2 from dad's age to doubleAge variable 
+//can simply use ternary operator with condition -> if doubleAge is less than zero (-doubleAge) ? return doubleAge * -1 (in order to return the years in a postive number)
+//if doubleAge > 0, then doubleAge will be returned
