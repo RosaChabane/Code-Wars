e084d457bfed59dfecc5e789c7307d30cae762a5
Rosa Chabane <rmiliziano@yahoo.com>
2023-09-28 12:04:17 -0500

    added all-star-code-challenge 8-kyu solution

---
+++ 8-kyu/All-Star-Code-Challenge.js
@@ -0,0 +1,30 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+//My Solution:
+
+function strCount(str, letter){  
+    return str.split("").filter(el => el === letter).length;
+}
+
+//My Explanation:
+//split the string into an array seperated by each letter
+//filter through that array to return elements that are equal to letter
+//then return the length of that filtered array
+
+
+//Tests:
+strCount('Hello', 'o') // 1
+strCount('Hello', 'l') // 2
+strCount('',      'z') // 0
