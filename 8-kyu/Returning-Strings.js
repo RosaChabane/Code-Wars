270b5c64db4f8568ccb292574f995667becbdf10
Rosa Chabane <rmiliziano@yahoo.com>
2024-04-23 17:56:28 -0500

    added 8-kyu returning-strings challenge solution

---
+++ 8-kyu/Returning-Strings.js
@@ -0,0 +1,17 @@
+// Make a function that will return a greeting statement that uses an input; 
+// your program should return, "Hello, <name> how are you doing today?".
+
+
+//My Solution:
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
+
+//My Explanation:
+//simply return the string and use template literal for name variable 
+
+//Tests:
+greet("Ryan"); // "Hello, Ryan how are you doing today?"
+
+
