c37fe4310392dc6c8551b4fc3951f589688f1a3f
Rosa Chabane <rmiliziano@yahoo.com>
2024-09-08 21:09:29 -0500

    added 8kyu make-uppercase solution

---
+++ 8-kyu/MakeUpperCase.js
@@ -0,0 +1,14 @@
+// Write a function which converts the input string to uppercase.
+
+//My Solution:
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+
+//Tests:
+makeUpperCase(""); //  ""
+makeUpperCase("hello"); // "HELLO"
+makeUpperCase("Hello"); // "HELLO"
+makeUpperCase("HELLO"); // "HELLO"
